19e9ca50b53fe74840240200c7f2478c8610f541
Timothy Nicolas <timothynicolas24@gmail.com>
2025-05-30 09:01:30 +0800

    Commit #3: Created About Section

--- index.php
+++ index.php
@@ -55,6 +55,20 @@
 
             </section>
 
+            <section class="about-image-container">
+                <img class="hero-image" src="assets/about-image.svg">
+            </section>
+            
+            <section class="about-text-container">
+                
+                <p class="secondary-header">We are <span class="primary-header underline">Kaidevs</span></p>
+
+                <p class="subheader"></p>At Kaidevs, we believe in the principle of Kaizen—a Japanese philosophy that means “continuous improvement.” We apply this mindset to every line of code, every user interface, and every client experience.</p>
+                <br>
+
+                <p class="subheader"></p>We’re not just developers—we’re growth partners. Whether you're a startup, a small business, or an enterprise, we build websites that adapt to your goals and scale with your success.</p>
+            </section>
+
             </div>
 
 
